bb7f4923a2d7816c894caab65683a389d026ec12
DanielAlvite <Daniskeleton99@gmail.com>
2023-09-04 13:54:20 +0200

    SECOND COMMIT - CREATION AND INPLEMENTATION OF USER

---
+++ src/main/java/org/campusdual/User.java
@@ -0,0 +1,43 @@
+package org.campusdual;
+import java.util.ArrayList;
+import java.util.List;
+public class User {
+
+    private String name;
+    private List<User> follows;
+    private List<Post> posts;
+
+    public User(String name){
+        this.name=name;
+        this.follows=new ArrayList<>();
+        this.posts = new ArrayList<>();
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void followUser(User user){
+        follows.add(user);
+    }
+
+    public void unFollowUser(User user){
+        follows.remove(user);
+    }
+    public void createPost(Post post) {
+        posts.add(post);
+    }
+
+    public void deletePost(Post post) {
+        posts.remove(post);
+    }
+    public List<Post> getPosts() {
+        return posts;
+    }
+    public List<Comment> getComments() {
+        List<Comment> comments = new ArrayList<>();
+        for (Post post : posts) {
+            comments.addAll(post.getComments());
+        }
+        return comments;
+    }
+}
