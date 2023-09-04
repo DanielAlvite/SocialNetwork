6ae263de9150920329025ad69a0a0152a9c55b84
DanielAlvite <Daniskeleton99@gmail.com>
2023-09-04 14:23:28 +0200

    FOURTH COMMIT - CREATION AND INPLEMENTATION OF Comment

---
+++ src/main/java/org/campusdual/Comment.java
@@ -0,0 +1,25 @@
+package org.campusdual;
+import java.util.Date;
+public class Comment {
+    private String text;
+    private Date date;
+
+    private User owner;
+
+    public Comment(String text,User owner){
+        this.text = text;
+        this.date = new Date();
+        this.owner = owner;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+    public User getOwner(){
+        return owner;
+    }
+}
